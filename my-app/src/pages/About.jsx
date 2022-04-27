842ac091c12262d7001a46ebac4a4d6b435fa991
uchenna <uchennachukwu2002@yahoo.com>
2022-04-27 02:15:15 -0700

    first commit

--- my-app/src/pages/About.jsx
+++ my-app/src/pages/About.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React from 'react';
+import image1 from '../assets/img/man.png'
+import image2 from '../assets/img/founder.png'
+import image3 from '../assets/img/manager.png'
 
 const About = () => {
-  return <div>About</div>;
-};
+  return (
 
-export default About;
+      <div className='container'>
+
+            <div className='first'>
+              <img src={image1} alt="" className='img'/> 
+
+          <br /><br /><br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus incidunt modi suscipit amet itaque nemo delectus harum voluptatem sed expedita.
+             </div> <br />
+
+
+             <div className='first'>
+              <img src={image2} alt="" className='img2'/> 
+
+          <br /><br /><br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus incidunt modi suscipit amet itaque nemo delectus harum voluptatem sed expedita.
+             </div> <br />
+
+             <div className='first'>
+              <img src={image3} alt="" className='img'/> 
+
+          <br /><br /><br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus incidunt modi suscipit amet itaque nemo delectus harum voluptatem sed expedita.
+             </div> 
+    </div>
+
+
+  )
+}
+
+export default About
