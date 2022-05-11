6d0151d189fe40d9807e460a38741db30cc90caf
Jamesalehandro <jamesalehandro@gmail.com>
2022-05-11 23:35:44 +0100

    Updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate('/dashboard');
+  };
+  return (
+    <div>
+      Login
+      {/* This is temporary to enable me access the dashboard. It will be removed when the login page is finished */}
+      <Button onClick={handleNavigate}>DashBoard</Button>
+    </div>
+  );
 };
 
 export default Login;
